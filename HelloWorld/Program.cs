890f1450ce1e0e5453ccd21e3cfefb08fcb21625
rohitmahor <rohitmahor09876@gmail.com>
2019-06-16 10:27:20 +0530

    first commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Sign(0));
+            
+        }
+    }
+}
